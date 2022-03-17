3777db87af34fb951cf6ea884281e12247faf2e8
Hadasss <hadas.gadish@gmail.com>
2022-03-17 11:56:20 -0700

    added post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
 
 // give prefix to all api urls in userRoutes
 router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
 
 module.exports = router;
+
+// FYI this file manages and assigns the correct url to each route.
