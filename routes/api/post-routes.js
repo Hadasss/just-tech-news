c73162f11b036119ac235c78575168bf4c1eae58
Hadasss <hadas.gadish@gmail.com>
2022-03-18 10:05:11 -0700

    added comment relational to user and post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,18 +1,38 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const sequelize = require("../../config/connection");
+const { Post, User, Vote, Comment } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("=====================");
   Post.findAll({
     // query configuration
-    attributes: ["id", "post_url", "title", "created_at"],
     order: [["created_at", "DESC"]],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          `(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)`
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       // this is the "JOIN" from another table
       {
         model: User,
         attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
       },
     ],
   })
@@ -28,17 +48,37 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          `(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)`
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
         attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
       },
     ],
   })
     .then((dbPostData) => {
       if (!dbPostData) {
         res.status(400).json({ message: "No post was found with this id" });
+        return;
       }
       res.json(dbPostData);
     })
@@ -64,6 +104,16 @@ router.post("/", (req, res) => {
     });
 });
 
+// this PUT route should be defined before the /:id PUT route. Otherwise, Express.js will think the word "upvote" is a valid parameter for /:id.
+router.put("/upvote", (req, res) => {
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 router.put("/:id", (req, res) => {
   Post.update(
     {
@@ -77,7 +127,7 @@ router.put("/:id", (req, res) => {
   )
     .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).res.json({ message: "No post was found with this id" });
+        res.status(404).json({ message: "No post was found with this id" });
         return;
       }
       res.json(dbPostData);
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../../models/User");
+const { Post, User, Vote, Comment } = require("../../models");
 
 // GET /api/users
 router.get("/", (req, res) => {
@@ -21,6 +21,26 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_url", "created_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+      {
+        model: Post,
+        attributes: ["title"],
+        through: Vote,
+        as: "voted_posts",
+      },
+    ],
   })
     .then((dbUserData) => {
       if (!dbUserData) {
@@ -60,8 +80,6 @@ router.post("/login", (req, res) => {
       res.status(400).json({ message: "No user with that email address!" });
       return;
     }
-    // res.json({ user: dbUserData });
-
     // verify user
     const validPassword = dbUserData.checkPassword(req.body.password);
     if (!validPassword) {
