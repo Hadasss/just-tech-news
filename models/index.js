6bd5b7a55548411b7824a5c86dcf129718fc8c53
Hadasss <hadas.gadish@gmail.com>
2022-03-17 17:03:40 -0700

    refactor model methods

--- models/index.js
+++ models/index.js
@@ -10,4 +10,34 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+
+// the Vote table needs a row of data to be a unique pairing so that it knows which data to pull in when queried on. because the user_id and post_id pairings must be unique, we're protected from the possibility of a single user voting on one post multiple times.
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Vote, {
+  foreignKey: "post_id",
+});
+
 module.exports = { User, Post, Vote };
