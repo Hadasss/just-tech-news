c85c630c58c93468be194dded461a9cac6bc3dd8
Hadasss <hadas.gadish@gmail.com>
2022-03-22 14:30:09 -0700

    added mvc basic structure

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
 // collecting the packaged group of API endpoints and prefixing them with the path /api
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
